0f0a28a30cace2dc6def3ea393fc0c136ca7393f
TulKasTer <yuni.bastidas@gmail.com>
2025-03-18 19:42:24 -0700

    Version 2

---
+++ Programa.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int leght;
+    scanf("%i", &lenght);
+
+    int office = pow(2, lenght);
+    printf("%i", office);
+    return 0;
+}
